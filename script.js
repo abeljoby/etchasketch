c0bf729b7273bb64c582e2a5943491feaea1b4ee
Abel <abelmathew1504@gmail.com>
2024-10-03 11:49:33 +0530

    Added features for extra credit

--- script.js
+++ script.js
@@ -7,11 +7,25 @@ function grid() {
     container.replaceChildren();
     for (let i=0;i<(side*side);i++) {
         const square = document.createElement("div");
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = randomColor();
+            let op = e.target.style.opacity;
+            e.target.style.opacity = op + 0.1;
+            console.log(e.target.style.opacity);
+        });
+        // square.opacity = "0";
         square.className = "gridsquare";
         square.style.flex = "1";
         square.style.flexBasis = `calc(100% / ${side})`;
         container.appendChild(square);
     }
+}
+
+function randomColor() {
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*255);
+    const b = Math.floor(Math.random()*255);
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
 const container = document.querySelector("#container");
